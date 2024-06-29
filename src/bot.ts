61fe2c95a7ec45a419cb920fd0d806fa772d506c
amaar5670 <118035158+amaar5670@users.noreply.github.com>
2024-06-29 15:52:22 +0530

    Update bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -64,7 +64,7 @@ const createBot = (): void => {
 	});
 	bot.once('login', () => {
 		console.log(`AFKBot logged in ${bot.username}\n\n`);
-		bot.chat("/register abcdehyjkl")
+		bot.chat("/login abcdehyjkl")
 	});
 	
 };
